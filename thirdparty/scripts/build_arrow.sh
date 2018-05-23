ecd38f634c1457552df6edafccc5aac43bcaa0f1
salah-man <salah.liuyj@antfin.com>
2018-05-23 11:19:01 +0800

    update the arrow version

--- thirdparty/scripts/build_arrow.sh
+++ thirdparty/scripts/build_arrow.sh
@@ -45,7 +45,7 @@ if [[ ! -d $TP_DIR/../python/ray/pyarrow_files/pyarrow ]]; then
     echo "building arrow"
 
     if [[ ! -d $TP_DIR/build/arrow ]]; then
-      git clone https://github.com/ant-tech-alliance/arrow.git "$TP_DIR/build/arrow"
+      git clone https://github.com/salah-man/arrow.git "$TP_DIR/build/arrow"
     fi
 
     pushd $TP_DIR/build/arrow
@@ -54,7 +54,7 @@ if [[ ! -d $TP_DIR/../python/ray/pyarrow_files/pyarrow ]]; then
     # include the link here to make it easier to find the right commit because
     # Arrow often rewrites git history and invalidates certain commits.
     #git checkout 5f9cf9c96709f92e9ac4828cf3e106a165576ce7
-    git checkout -b java_lib origin/java_lib
+    git checkout -b java_lib_0523 origin/java_lib_0523
 
     cd cpp
     if [ ! -d "build" ]; then
